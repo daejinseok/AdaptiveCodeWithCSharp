30e082883ba22e75a68997fd29e58e8b711a3cc8
Derik Jang <aspnetmvp@gmail.com>
2015-07-05 12:52:44 +1000

    Ch9 WPF sample - Convert column headers and data to Korean

--- Chapter9/ch9-dependency-inject-base-wpf/DependencyInjectionWpf/TaskDatabase/seed_data.sql
+++ Chapter9/ch9-dependency-inject-base-wpf/DependencyInjectionWpf/TaskDatabase/seed_data.sql
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Post-Deployment Script Template							
 --------------------------------------------------------------------------------------
  This file contains SQL statements that will be appended to the build script.		
@@ -10,15 +10,14 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-INSERT INTO task VALUES(0, 'Clean the house', 'LOW', DATEADD(d, 1, GETDATE()), 0);
-INSERT INTO task VALUES(1, 'Pay the bills', 'HIGH', DATEADD(d, 2, GETDATE()), 0);
-INSERT INTO task VALUES(2, 'Wash the dog', 'MED', DATEADD(d, 3, GETDATE()), 0);
-INSERT INTO task VALUES(3, 'Book flights', 'HIGH', DATEADD(d, 3, GETDATE()), 0);
-INSERT INTO task VALUES(4, 'Buy presents', 'HIGH', DATEADD(d, 4, GETDATE()), 0);
-INSERT INTO task VALUES(5, 'Post letters', 'MED', DATEADD(d, 4, GETDATE()), 0);
-INSERT INTO task VALUES(6, 'Write emails', 'LOW', DATEADD(d, 4, GETDATE()), 0);
-INSERT INTO task VALUES(7, 'Read articles', 'LOW', DATEADD(d, 5, GETDATE()), 0);
-INSERT INTO task VALUES(8, 'Skype family', 'HIGH', DATEADD(d, 5, GETDATE()), 0);
-INSERT INTO task VALUES(9, 'Take out wife', 'HIGH', DATEADD(d, 6, GETDATE()), 0);
-INSERT INTO task VALUES(10, 'Write book', 'HIGH', DATEADD(m, 1, GETDATE()), 0);
-
+INSERT INTO task VALUES(0, '집 청소하기', '낮음', DATEADD(d, 1, GETDATE()), 0);
+INSERT INTO task VALUES(1, '공과금 납부하기', '높음', DATEADD(d, 2, GETDATE()), 0);
+INSERT INTO task VALUES(2, '강아지 목욕시키기', '보통', DATEADD(d, 3, GETDATE()), 0);
+INSERT INTO task VALUES(3, '항공권 예매하기', '높음', DATEADD(d, 3, GETDATE()), 0);
+INSERT INTO task VALUES(4, '선물 구입하기', '높음', DATEADD(d, 4, GETDATE()), 0);
+INSERT INTO task VALUES(5, '소포 부치기', '보통', DATEADD(d, 4, GETDATE()), 0);
+INSERT INTO task VALUES(6, '메일 쓰기', '낮음', DATEADD(d, 4, GETDATE()), 0);
+INSERT INTO task VALUES(7, '신문 기사 읽기', '낮음', DATEADD(d, 5, GETDATE()), 0);
+INSERT INTO task VALUES(8, '부모님과 스카이프 통화', '높음', DATEADD(d, 5, GETDATE()), 0);
+INSERT INTO task VALUES(9, '집사람과 외출하기', '높음', DATEADD(d, 6, GETDATE()), 0);
+INSERT INTO task VALUES(10, '책 집필 하기', '높음', DATEADD(m, 1, GETDATE()), 0);
