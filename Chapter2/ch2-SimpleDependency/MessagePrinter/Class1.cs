eb86364eec73681c39d9282f6d6b54ca709e98e9
daejin <daejin.seok@gmail.com>
2015-10-09 15:55:49 +0900

    MessagePritnService Class 추가
    MessagePritnService Class가 책본문이나 Jpub Github에 없어, 책본문 대로 테스트가능하게 작성

---
+++ Chapter2/ch2-SimpleDependency/MessagePrinter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessagePrinter
+{
+    public class MessagePrintingService
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Call MessagePrint");
+        }
+    }
+}
---
+++ Chapter2/ch2-SimpleDependency/SimpleDependency/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MessagePrinter;
+
+namespace SimpleDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new MessagePrintingService();
+            service.PrintMessage();
+            Console.ReadKey();
+        }
+    }
+}
